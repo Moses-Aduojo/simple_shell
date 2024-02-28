dda2865b32e5478733c88400ec3fb25c806a31ff
Moses-Aduojo <temojokego@gmail.com>
2024-02-28 12:02:10 -0800

    re-editted

--- exec_cmd.c
+++ exec_cmd.c
@@ -1,5 +1,6 @@
 #include "shell.h"
 
+extern char **environ;
 /**
  * exec_cmd - execute other program pass to it as command
  * @exec_args: pointer to array of command and its argument
@@ -7,7 +8,7 @@
  */
 void exec_cmd(char **exec_args)
 {
-	if (execve(exec_args[0], exec_args, NULL) == -1)
+	if (execve(exec_args[0], exec_args, environ) == -1)
 	{
 		perror(exec_args[0]);
 		exit(EXIT_FAILURE);
